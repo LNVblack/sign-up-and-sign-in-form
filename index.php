0b620df3a4ac6f93ec061883f9528f015b975351
Nik <tshl20141@gmail.com>
2020-10-07 16:33:11 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,31 @@
+<?php require "db.php";?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href = "style.css">
     <title>Главная</title>
 </head>
 <body>
     <header>
-        <a href="/signup.php">Регистрация</a>
-        <a href="/signin.php">Войти</a>
+        <a class="status_f" href="/signup.php">Регистрация</a>
+        <a class="status_f" href="/signin.php">Войти</a>
+        <a class="status_t" href="signout.php">Выйти</a>
     </header>
+    <?php 
+    if(isset($_SESSION['verfication_user'])){
+        echo '<style>.status_t{display:block;}</style>';
+    }else{
+        echo '<style>.status_f{display:block;}</style>';
+    }
+    ?>
+    <main>
+        <div class="status_t infoblock">
+            <p>Вы авторизованы!</p>
+        </div>
+        <div class="status_f infoblock">
+            <p>Вы не авторизованы!</p>
+        </div>
+    </main>
 </body>
 </html>
