f1379d8a2f7022e41cd438131fc459a427bb929c
Nik <tshl20141@gmail.com>
2020-10-07 16:32:53 +0300

    edit

--- db.php
+++ db.php
@@ -1,6 +1,5 @@
-<?php
+<?php session_start();
     require "libs/rb-mysql.php";
     R::setup( 'mysql:host=server236.hosting.reg.ru;dbname=u0650758_username',
         'u0650758_admin', 'admin@1' );
-//https://server236.hosting.reg.ru/phpmyadmin Ссылка к бд
 ?>
